fdd0e4e5b25c6fe5bfe3cb3be173f8c1f43d7e99
pidupuis-sec <pidupuisjs@gmail.com>
2022-07-01 20:03:21 +0000

    fix server code

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -15,10 +15,9 @@ variables that you set up in the .env file*/
 const dotenv = require('dotenv');
 dotenv.config();
 
-const api_key = process.env.API_KEY;
-const api_url = process.env.API_URL;
-
 function getNLUInstance() {
+    const api_key = process.env.API_KEY;
+    const api_url = process.env.API_URL;
     const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
     const { IamAuthenticator } = require('ibm-watson/auth');
 
